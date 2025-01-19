05d14589d86f035e0e206937d5e76f8201114bad
joaocarbonell <joaocarbonellpc@gmail.com>
2025-01-19 18:00:37 -0300

    docs: Add python code documentation
    Added code documention to the update attendance method on the attendance controller

--- kpi-automation-api/app/controllers/attendance_controller.py
+++ kpi-automation-api/app/controllers/attendance_controller.py
@@ -19,9 +19,24 @@ class AttendanceController:
 
     @staticmethod
     def update_attendance(data, id):
+        """
+        Updates attendance records for a specific user.
+
+        This method takes in attendance data and an identifier, validates the data using the
+        `AttendanceSchema`, and updates attendance records by utilizing the
+        `AttendanceService.update_attendance` function. If validation fails or other exceptions arise,
+        appropriate error responses are returned.
+
+        :param data: A dictionary containing attendance update data to be validated.
+        :param id: An identifier for the user whose attendance is to be updated.
+        :return: An HTTP response object. On success, returns the updated attendance data.
+            On failure, returns an error message with a corresponding HTTP status code.
+        """
         try:
             schema = AttendanceSchema()
+            # Validating the data sent from the client
             validated_data = schema.load(data)
+            # Sending data to the service
             return AttendanceService.update_attendance(validated_data, id)
         except ValidationError as ve:
             return make_response(jsonify({'message': 'INVALID_DATA', 'errors': ve.messages}), 400)
